34decbc84e3e7b4f3e0a87582f09593a41644697
FrancescoBossoli <113616058+FrancescoBossoli@users.noreply.github.com>
2023-03-14 19:11:48 +0100

    Standard Jwt & Lazy-Loaded Routes Implementation

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+   { path: 'signup', loadChildren: () => import('./modules/signup/signup.module').then(m => m.SignupModule) },
+   { path: 'signup', loadChildren: () => import('./modules/signup/signup.module').then(m => m.SignupModule) },
+   { path: 'error', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]
 })
 export class AppRoutingModule { }
