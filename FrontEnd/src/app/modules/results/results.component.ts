6d3c5f63abfbf5c7bb7c49584d6cfc791009d045
FrancescoBossoli <113616058+FrancescoBossoli@users.noreply.github.com>
2023-04-04 16:54:23 +0200

    fix on listings collection / marker visualization

--- FrontEnd/src/app/modules/results/results.component.ts
+++ FrontEnd/src/app/modules/results/results.component.ts
@@ -19,42 +19,39 @@ export class ResultsComponent implements OnInit{
    emptyUser:User = Object.assign({});
    user:User = Object.assign({});
    isLogged:boolean = false;
+   lat = 0;
+   long = 0;
 
    map!: mapboxgl.Map;
    style = 'mapbox://styles/mapbox/streets-v11';
+   markerCollection:mapboxgl.Marker[] = [];
 
    constructor(private route: ActivatedRoute, private listServ: ListingService, private authSrv: AuthService) { }
 
    ngOnInit(): void {
       this.authSrv.user$.subscribe((res) => this.user = {...this.emptyUser, ...res} );
       this.authSrv.isLoggedIn$.subscribe((res) => this.isLogged = res);
-      let lat = 0;
-      let long = 0;
-      this.map = new mapboxgl.Map({
-         accessToken: environment.mapToken,
-         container: 'map',
-         style: this.style,
-         zoom: 13
-      });
+      this.resetMap();
       if (window.location.pathname == '/results') {
          this.listServ.results$.pipe(delay(0)).subscribe((res) => {
             this.listings = res;
+            this.resetMap();
             res.forEach((listing) => {
                // this.listings.push(listing)
-               lat += listing.latitude;
-               long += listing.longitude;
+               this.lat += listing.latitude;
+               this.long += listing.longitude;
                this.setMarker(listing);
             })
-            this.map.setCenter([long/this.listings.length, lat/this.listings.length]);
+            this.map.setCenter([this.long/this.listings.length, this.lat/this.listings.length]);
          });
       } else {
          this.listings = this.user.favourites;
          this.listings.forEach((listing) => {
-            lat += listing.latitude;
-            long += listing.longitude;
+            this.lat += listing.latitude;
+            this.long += listing.longitude;
             this.setMarker(listing);
          })
-         this.map.setCenter([long/this.listings.length, lat/this.listings.length]);
+         this.map.setCenter([this.long/this.listings.length, this.lat/this.listings.length]);
       }
       this.map.on('click', (e) => this.map.setCenter(e.lngLat))
    }
@@ -69,6 +66,7 @@ export class ResultsComponent implements OnInit{
       .setLngLat([listing.longitude, listing.latitude])
       .setPopup(new mapboxgl.Popup({focusAfterOpen: false, anchor:'center'}).setHTML("<div style='width:160px;'><div class='ratio ratio-4x3'><a href=\"/listing/" + listing.id + "\" class='ratio ratio-4x3'><img src='" + listing.pictures[0] + "' class='rounded-4'></a></div><p class='text-truncate text-black mb-0'>" + listing.name + "</p><div class='d-flex justify-content-between'><p class='mb-0 text-black'><span class='fw-semibold'>€" + listing.price + "</span>/notte</p><p class='me-1 mb-0 text-black'><i class='fa-sharp fa-solid fa-star-sharp'></i>" + (avg/listing.reviews.length).toFixed(2) + "</p></div></div>"))
       .addTo(this.map);
+      this.markerCollection.push(marker);
 
       const el = document.createElement('div');
       el.className = 'marker';
@@ -82,6 +80,18 @@ export class ResultsComponent implements OnInit{
       el.style.lineHeight = '38px';
       el.textContent = listing.price.toString() + '€';
       marker.getElement().firstChild?.replaceWith(el);
+   }
+
+   resetMap() {
+      this.lat = 0;
+      this.long = 0;
+      this.map = new mapboxgl.Map({
+         accessToken: environment.mapToken,
+         container: 'map',
+         style: this.style,
+         zoom: 13
+      });
+      this.markerCollection.forEach((marker) => marker.remove());
    }
 
 
